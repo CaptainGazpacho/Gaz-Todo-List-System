68f630813943f8078f71dccd39e8666d14ca5ac2
Captain-Gazpacho <lightningcyanmcts@gmail.com>
2026-09-06 11:13:52 -0500

    Cleaned up the DatabaseManager

--- todo_list_app/src/main/java/com/gaz/todolist/databaseManager.java
+++ todo_list_app/src/main/java/com/gaz/todolist/databaseManager.java
@@ -20,12 +20,13 @@ public class databaseManager {
      * Creates a new database file if it does not already exist
      */
     public static void createDatabase() {
+
+        String[] listTables = {"TODO_LIST", "MANUAL_TODO_LIST", "PLAN_TODO_LIST", "DELEGATE_TODO_LIST", "HOLD_TODO_LIST", "RECURRING_TODO_LIST", "COMPLETE_TODO_LIST"};
+
         try {
             File myObj = new File("todo_list_app\\sql\\todo_list.db");
             if (myObj.createNewFile()) {
-                //System.out.println("File created: " + myObj.getName());
             } else {
-                //System.out.println("File already exists.");
             }
         } catch (IOException e) {
             System.out.println("An error occurred.");
@@ -34,139 +35,31 @@ public class databaseManager {
 
         String url = "jdbc:sqlite:todo_list_app\\sql\\todo_list.db";
 
-        String createTodoListTableQuery = """
-                CREATE TABLE IF NOT EXISTS TODO_LIST (
-	                "ACCOUNT"	TEXT,
-	                "TASK_ID"	TEXT PRIMARY KEY,
-	                "RANK"	INTEGER,
-	                "TASK"	TEXT,
-	                "DEADLINE"	INTEGER,
-	                "SCHEDULED_TIME"	TEXT,
-	                "MANUAL"	TEXT,
-	                "RECURRING"	TEXT,
-	                "SIZE"	INTEGER,
-	                "STATUS"	INTEGER
-                );
-                """;
+        for(String tableName:listTables) {
+            String createTableQuery = """
+                    CREATE TABLE IF NOT EXISTS %s (
+                        "ACCOUNT"	TEXT,
+                        "TASK_ID"	TEXT PRIMARY KEY,
+                        "RANK"	INTEGER,
+                        "TASK"	TEXT,
+                        "DEADLINE"	INTEGER,
+                        "SCHEDULED_TIME"	TEXT,
+                        "MANUAL"	TEXT,
+                        "RECURRING"	TEXT,
+                        "SIZE"	INTEGER,
+                        "STATUS"	INTEGER
+                    );
+                    """.formatted(tableName);
 
-        String createManualTodoListTableQuery = """
-                CREATE TABLE IF NOT EXISTS MANUAL_TODO_LIST (
-	                "ACCOUNT"	TEXT,
-	                "TASK_ID"	TEXT PRIMARY KEY,
-	                "RANK"	INTEGER,
-	                "TASK"	TEXT,
-	                "DEADLINE"	INTEGER,
-	                "SCHEDULED_TIME"	TEXT,
-	                "MANUAL"	TEXT,
-	                "RECURRING"	TEXT,
-	                "SIZE"	INTEGER,
-	                "STATUS"	INTEGER
-                );
-                """;
-
-        String createPlanTodoListTableQuery = """
-                CREATE TABLE IF NOT EXISTS PLAN_TODO_LIST (
-	                "ACCOUNT"	TEXT,
-	                "TASK_ID"	TEXT PRIMARY KEY,
-	                "RANK"	INTEGER,
-	                "TASK"	TEXT,
-	                "DEADLINE"	INTEGER,
-	                "SCHEDULED_TIME"	TEXT,
-	                "MANUAL"	TEXT,
-	                "RECURRING"	TEXT,
-	                "SIZE"	INTEGER,
-	                "STATUS"	INTEGER
-                );
-                """;
-
-        String createDelegateTodoListTableQuery = """
-                CREATE TABLE IF NOT EXISTS DELEGATE_TODO_LIST (
-	                "ACCOUNT"	TEXT,
-	                "TASK_ID"	TEXT PRIMARY KEY,
-	                "RANK"	INTEGER,
-	                "TASK"	TEXT,
-	                "DEADLINE"	INTEGER,
-	                "SCHEDULED_TIME"	TEXT,
-	                "MANUAL"	TEXT,
-	                "RECURRING"	TEXT,
-	                "SIZE"	INTEGER,
-	                "STATUS"	INTEGER
-                );
-                """;
-
-        String createHoldTodoListTableQuery = """
-                CREATE TABLE IF NOT EXISTS HOLD_TODO_LIST (
-	                "ACCOUNT"	TEXT,
-	                "TASK_ID"	TEXT PRIMARY KEY,
-	                "RANK"	INTEGER,
-	                "TASK"	TEXT,
-	                "DEADLINE"	INTEGER,
-	                "SCHEDULED_TIME"	TEXT,
-	                "MANUAL"	TEXT,
-	                "RECURRING"	TEXT,
-	                "SIZE"	INTEGER,
-	                "STATUS"	INTEGER
-                );
-                """;
-
-        String createRecurringTodoListTableQuery = """
-                CREATE TABLE IF NOT EXISTS RECURRING_TODO_LIST (
-	                "ACCOUNT"	TEXT,
-	                "TASK_ID"	TEXT PRIMARY KEY,
-	                "RANK"	INTEGER,
-	                "TASK"	TEXT,
-	                "DEADLINE"	INTEGER,
-	                "SCHEDULED_TIME"	TEXT,
-	                "MANUAL"	TEXT,
-	                "RECURRING"	TEXT,
-	                "SIZE"	INTEGER,
-	                "STATUS"	INTEGER
-                );
-                """;
-
-        String createCompleteTodoListTableQuery = """
-                CREATE TABLE IF NOT EXISTS COMPLETE_TODO_LIST (
-	                "ACCOUNT"	TEXT,
-	                "TASK_ID"	TEXT PRIMARY KEY,
-	                "RANK"	INTEGER,
-	                "TASK"	TEXT,
-	                "DEADLINE"	INTEGER,
-	                "SCHEDULED_TIME"	TEXT,
-	                "MANUAL"	TEXT,
-	                "RECURRING"	TEXT,
-	                "SIZE"	INTEGER,
-	                "STATUS"	INTEGER
-                );
-                """;
-
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                PreparedStatement createTodoListStmt = conn.prepareStatement(createTodoListTableQuery);
-                createTodoListStmt.execute();
-
-                PreparedStatement createManualTodoListStmt = conn.prepareStatement(createManualTodoListTableQuery);
-                createManualTodoListStmt.execute();
-
-                PreparedStatement createPlanTodoListStmt = conn.prepareStatement(createPlanTodoListTableQuery);
-                createPlanTodoListStmt.execute();
-
-                PreparedStatement createDelegateTodoListStmt = conn.prepareStatement(createDelegateTodoListTableQuery);
-                createDelegateTodoListStmt.execute();
-
-                PreparedStatement createHoldTodoListStmt = conn.prepareStatement(createHoldTodoListTableQuery);
-                createHoldTodoListStmt.execute();
-
-                PreparedStatement createRecurringTodoListStmt = conn.prepareStatement(createRecurringTodoListTableQuery);
-                createRecurringTodoListStmt.execute();
-
-                PreparedStatement createCompleteTodoListStmt = conn.prepareStatement(createCompleteTodoListTableQuery);
-                createCompleteTodoListStmt.execute();
-
-                conn.close();
-
+            try (Connection conn = DriverManager.getConnection(url)) {
+                if (conn != null) {
+                    PreparedStatement createTableStmt = conn.prepareStatement(createTableQuery);
+                    createTableStmt.execute();
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
         }
     }
 
@@ -228,10 +121,10 @@ public class databaseManager {
         String url = "jdbc:sqlite:todo_list_app\\sql\\todo_list.db";
 
         String mergeQuery = """
-                INSERT OR REPLACE INTO """ + table + """
+                INSERT OR REPLACE INTO %s
                     (ACCOUNT, TASK_ID, RANK, TASK, DEADLINE, SCHEDULED_TIME, MANUAL, RECURRING, SIZE, STATUS)
                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-                """;
+                """.formatted(table);
 
         try (Connection conn = DriverManager.getConnection(url)) {
             if (conn != null) {
